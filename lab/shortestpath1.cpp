356af2f2ab92fed16cfce4b26b606e452863de52
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2025-05-16 10:37:24 +0200

    lab2 dijkstras

---
+++ lab/shortestpath1.cpp
@@ -0,0 +1,273 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <queue>
+
+/**
+ * @author Oskar Arensmeier
+ * @date 2025-05-15
+ */
+
+/**
+ * Implementation of Dijkstra's algorithm
+ *
+ * @note Assumes that no edge has negative weight!
+ *
+ * Algorithm:
+ *
+ * Standard Dijkstra's algorithm using a priority queue.
+ * We start from the source node and explore all neighbors.
+ * If we find a path to a neighbor that is shorter than what we've
+ * already found, we update the distance and add the neighbor to the queue.
+ *
+ * When we reach the end node, we backtrack to find the shortest path.
+ *
+ *
+ * Time complexity:
+ *
+ * O((n + m) * log n)
+ * where n is the number of nodes, and m is the number of edges
+ *
+ *
+ * Space complexity:
+ *
+ * - O(n + m) for the graph
+ * - O(n) for the distance and previous node arrays
+ * - O(m) for the priority queue
+ * In total: O(n + m)
+ * 
+ * 
+ * Data structures:
+ *
+ * - Adjacency list using vectors
+ * - Priority queue for the Dijkstra's algorithm
+ */
+
+using namespace std;
+
+using ll = long long int;
+using llu = long long unsigned;
+
+/**
+ * Models a directed edge
+ */
+class Edge
+{
+public:
+
+    /**
+     * To node
+     */
+    int v;
+
+    /**
+     * Weight
+     */
+    int w;
+
+    Edge(int v, int w) : v(v), w(w) {}
+};
+
+class Graph
+{
+public:
+
+    /**
+     * Node count
+     */
+    int N;
+
+    /**
+     * Edge count
+     */
+    int M;
+
+    /**
+     * Adjacency list
+     */
+    vector<vector<Edge>> adj;
+
+    /**
+     * Infinity
+     */
+    static constexpr int INF = numeric_limits<int>::max();
+
+    /**
+     * Constructor
+     * @param n Number nodes
+     * @param m Number edges
+     */
+    Graph(int n, int m) : N(n), M(m)
+    {
+        adj.resize(N, vector<Edge>());
+    }
+
+    /**
+     * Add edge from u to v with weight w
+     * @param u Node u
+     * @param v Node v
+     * @param w Weight
+     */
+    void add_edge(int u, int v, int w)
+    {
+        adj[u].push_back(Edge(v, w));
+    }
+
+    /**
+     * Get neighbors of u
+     * @param u Node u
+     * @return Vector neighbors
+     */
+    const vector<Edge> & neighbors(int u) const
+    {
+        return adj[u];
+    }
+};
+
+class Dijkstra
+{
+public:
+
+    /**
+     * Graph
+     */
+    Graph G;
+
+    /**
+     * Distance from start to each node
+     */
+    vector<int> dist;
+
+    /**
+     * Previous node in the path
+     */
+    vector<int> prev;
+
+    /**
+     * Start node
+     */
+    int start;
+
+    /**
+     * Constructor
+     * @param G Graph
+     * @param start Start node
+     */
+    Dijkstra(Graph && G, int start) : G(move(G)), start(start)
+    {
+        dist.resize(this->G.N, G.INF);
+        prev.resize(this->G.N, -1);
+    }
+
+    /**
+     * Run Dijkstra's algorithm
+     */
+    void run()
+    {
+        dist[start] = 0;
+
+        // Priority queue with pair <distance, node>
+        // Elements priority sorted by distance
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            auto [d, u] = pq.top();
+            pq.pop();
+
+            // Skip if we've already seen a shorter path
+            if (d > dist[u]) {
+                continue;
+            }
+
+            for (auto e : G.neighbors(u)) {
+                int alt = dist[u] + e.w;
+
+                if (alt < dist[e.v]) {
+                    dist[e.v] = alt;
+                    prev[e.v] = u;
+                    pq.push({alt, e.v});
+                }
+            }
+        }
+    }
+
+    /**
+     * Get shortest path from start to end
+     *
+     * @param end End node
+     * @return Vector path
+     * @throws std::runtime_error if no path exists
+     */
+    vector<int> shortest_path(int end)
+    {
+        vector<int> path;
+        for (int v = end; v != -1; v = prev[v]) {
+            path.push_back(v);
+        }
+        reverse(path.begin(), path.end());
+
+        // This means that start and end
+        // nodes are not connected
+        if (path.size() == 0 || path[0] != start) {
+            throw std::runtime_error("No path found");
+        }
+
+        return path;
+    }
+
+    /**
+     * Get distance from start to end
+     *
+     * @param end End node
+     * @return Distance
+     * @note Returns INF if no path exists
+     */
+    int get_distance(int end)
+    {
+        return dist[end];
+    }
+};
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, q, s;
+    while (1) {
+        cin >> n >> m >> q >> s;
+        if (n == 0 && m == 0 && q == 0 && s == 0) {
+            break;
+        }
+
+        Graph G(n, m);
+
+        int u, v, w;
+        for (int i = 0; i < m; i++) {
+            cin >> u >> v >> w;
+            G.add_edge(u, v, w);
+        }
+
+        Dijkstra dijkstra(move(G), s);
+        dijkstra.run();
+
+        for (int i = 0; i < q; i++) {
+            cin >> u;
+
+            if (dijkstra.get_distance(u) == Graph::INF) {
+                cout << "Impossible\n";
+            } else {
+                cout << dijkstra.get_distance(u) << "\n";
+            }
+        }
+    }
+
+    cout << flush;
+    return 0;
+}
